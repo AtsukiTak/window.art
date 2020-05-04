b20cd9d774b8f365bbdbecf5a50ae43fd097451c
AtsukiTak <takatomgoo@gmail.com>
2020-05-04 17:37:39 +0900

    add add_artist command

---
+++ server/artell-app/src/command/add_artist.rs
@@ -0,0 +1,22 @@
+use artell_domain::artist::{Artist, ArtistId, ArtistRepository};
+
+pub async fn add_artist<AR>(
+    name: String,
+    email: String,
+    status_msg: String,
+    description: String,
+    instagram: String,
+    twitter: String,
+    artist_repo: &AR,
+) -> anyhow::Result<ArtistId>
+where
+    AR: ArtistRepository,
+{
+    let artist = Artist::new(name, email, status_msg, description, instagram, twitter)?;
+
+    let artist_id = artist.id;
+
+    artist_repo.save(artist).await?;
+
+    Ok(artist_id)
+}
--- server/artell-app/src/lib.rs
+++ server/artell-app/src/lib.rs
@@ -1,4 +1,5 @@
 #[macro_use]
 extern crate serde;
 
+pub mod command;
 pub mod query;
