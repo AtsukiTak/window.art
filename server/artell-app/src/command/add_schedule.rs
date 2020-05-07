ef29a7fc96d6fef698121985682e58c95bc63b8a
AtsukiTak <takatomgoo@gmail.com>
2020-05-07 12:45:59 +0900

    add add_schedule use case

---
+++ server/artell-app/src/command/add_schedule.rs
@@ -0,0 +1,47 @@
+use artell_domain::{art::ArtRepository, scheduler::SchedulerRepository};
+use chrono::{DateTime, Utc};
+use uuid::Uuid;
+
+pub struct Params {
+    pub art_id: Uuid,
+    pub activate_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("art {0} is not found")]
+    ArtNotFound(Uuid),
+    #[error("scheduler is not initialized yet")]
+    SchedulerNotInitialized,
+    #[error(transparent)]
+    Others(#[from] anyhow::Error),
+}
+
+pub async fn add_schedule(
+    params: Params,
+    art_repo: impl ArtRepository,
+    scheduler_repo: impl SchedulerRepository,
+) -> Result<(), Error> {
+    let Params {
+        art_id,
+        activate_at,
+    } = params;
+
+    // Artが存在することを確認
+    let art = art_repo
+        .find_by_id(art_id)
+        .await?
+        .ok_or(Error::ArtNotFound(art_id))?;
+
+    let mut scheduler = scheduler_repo
+        .find()
+        .await?
+        .ok_or(Error::SchedulerNotInitialized)?;
+
+    scheduler.add_schedule(art.id, activate_at);
+    scheduler.check_update();
+
+    scheduler_repo.save(scheduler).await?;
+
+    Ok(())
+}
