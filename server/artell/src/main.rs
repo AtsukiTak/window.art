ea307fd0b4e34f077f3c9f4c92f260cf7a0fe9c0
AtsukiTak <takatomgoo@gmail.com>
2020-05-10 19:57:36 +0900

    impl server

--- server/artell/src/main.rs
+++ server/artell/src/main.rs
@@ -3,7 +3,20 @@ extern crate serde;
 
 pub mod res;
 pub mod routes;
+pub mod server;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let port = get_env_var_u16_or_panic("PORT");
+
+    server::bind(([0, 0, 0, 0], port)).await
+}
+
+fn get_env_var_or_panic(key: &'static str) -> String {
+    std::env::var(key).unwrap()
+}
+
+fn get_env_var_u16_or_panic(key: &'static str) -> u16 {
+    let s = get_env_var_or_panic(key);
+    u16::from_str_radix(s.as_str(), 10).unwrap()
 }
--- server/artell/src/server.rs
+++ server/artell/src/server.rs
@@ -1,14 +1,9 @@
-use crate::{api, html};
+use crate::routes;
 use std::net::SocketAddr;
 use warp::{filters::log, Filter};
 
-pub async fn bind(
-    socket: impl Into<SocketAddr> + 'static,
-    html_path: impl Into<std::path::PathBuf> + 'static,
-) {
-    let filter = api::filter()
-        .or(html::filter(html_path))
-        .with(log::log("crop"));
+pub async fn bind(socket: impl Into<SocketAddr> + 'static) {
+    let filter = routes::api::route().with(log::log("crop"));
 
     let server = warp::serve(filter);
     server.bind(socket).await
