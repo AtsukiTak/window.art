afd61ab61a36dfd558742cbf5ba3a0dd791ef37d
AtsukiTak <takatomgoo@gmail.com>
2020-05-10 19:08:01 +0900

    fix ImageRepository

--- server/artell-domain/src/image/repository.rs
+++ server/artell-domain/src/image/repository.rs
@@ -2,7 +2,7 @@ use super::Image;
 
 #[async_trait]
 pub trait ImageRepository {
-    fn url_to(&self, image: &Image) -> String;
+    fn url_to(&self, image_name: &str) -> String;
 
     async fn save(&self, image: Image) -> anyhow::Result<()>;
 }
