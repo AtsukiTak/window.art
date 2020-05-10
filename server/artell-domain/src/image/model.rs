5db0fbddf33d86cec8b0454efa7091a5807be1f7
AtsukiTak <takatomgoo@gmail.com>
2020-05-10 19:07:18 +0900

    use name field instead of id in Image model

--- server/artell-domain/src/image/model.rs
+++ server/artell-domain/src/image/model.rs
@@ -3,18 +3,12 @@ use uuid::Uuid;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Image {
-    pub id: ImageId,
+    pub name: String,
     pub data: Bytes,
-    pub format: ImageFormat,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct ImageId(pub Uuid);
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum ImageFormat {
-    Png,
-}
 
 #[derive(Error, Debug)]
 pub enum Error {
@@ -25,36 +19,20 @@ pub enum Error {
 }
 
 /*
- * ========
- * Query
- * ========
- */
-impl Image {
-    pub fn name(&self) -> String {
-        let extension = match self.format {
-            ImageFormat::Png => "png",
-        };
-
-        format!("{}.{}", self.id.0, extension)
-    }
-}
-
-/*
  * =======
  * Command
  * =======
  */
 impl Image {
     pub fn new(data: Bytes) -> Result<Self, Error> {
-        let format = match image::guess_format(&data)? {
-            image::ImageFormat::Png => ImageFormat::Png,
+        let format_str = match image::guess_format(&data)? {
+            image::ImageFormat::Png => "png",
             f => return Err(Error::UnsupportedFormat(f)),
         };
 
         Ok(Image {
-            id: ImageId(Uuid::new_v4()),
+            name: format!("{}.{}", Uuid::new_v4(), format_str),
             data,
-            format,
         })
     }
 }
