0a71de6fbd946404e739fc700675ddab74ccfa0f
AtsukiTak <takatomgoo@gmail.com>
2020-05-13 00:16:43 +0900

    update artell-cli

--- server/artell-cli/src/main.rs
+++ server/artell-cli/src/main.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use serde::Serialize;
 use structopt::StructOpt;
 use uuid::Uuid;
@@ -6,6 +7,7 @@ use uuid::Uuid;
 #[structopt(name = "admin-cli")]
 enum Command {
     AddArt(AddArtCommand),
+    AddSchedule(AddScheduleCommand),
 }
 
 #[tokio::main]
@@ -13,17 +15,20 @@ async fn main() {
     let command = Command::from_args();
     match command {
         Command::AddArt(cmd) => add_art(cmd).await,
+        Command::AddSchedule(cmd) => add_schedule(cmd).await,
     };
 }
 
+/*
+ * ===========
+ * AddArt
+ * ===========
+ */
 #[derive(Debug, StructOpt)]
 struct AddArtCommand {
     server_url: String,
-    #[structopt(short)]
     artist_id: Uuid,
-    #[structopt(short)]
     title: String,
-    #[structopt(short)]
     image_path: String,
 }
 
@@ -47,6 +52,42 @@ async fn add_art(cmd: AddArtCommand) {
 
     let res = reqwest::Client::new()
         .post(&format!("{}/api/v1/admin/add_art", cmd.server_url))
+        .json(&body)
+        .send()
+        .await
+        .unwrap();
+    println!("{:?}", res);
+    let txt = res.text().await;
+    println!("{:?}", txt);
+}
+
+/*
+ * ===========
+ * AddSchedule
+ * ===========
+ */
+#[derive(Debug, StructOpt)]
+struct AddScheduleCommand {
+    server_url: String,
+    art_id: Uuid,
+    activate_at: DateTime<Utc>,
+}
+
+async fn add_schedule(cmd: AddScheduleCommand) {
+    #[derive(Serialize)]
+    #[serde(rename_all = "camelCase")]
+    struct ReqBody {
+        art_id: Uuid,
+        activate_at: DateTime<Utc>,
+    }
+
+    let body = ReqBody {
+        art_id: cmd.art_id,
+        activate_at: cmd.activate_at,
+    };
+
+    let res = reqwest::Client::new()
+        .post(&format!("{}/api/v1/admin/add_schedule", cmd.server_url))
         .json(&body)
         .send()
         .await
