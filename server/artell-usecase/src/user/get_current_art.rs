be849ed2d0e55ef8d670b4d7b343ccfd00fac449
AtsukiTak <takatomgoo@gmail.com>
2020-05-13 14:09:22 +0900

    user::get_current_art use case return Artist as well

--- server/artell-usecase/src/user/get_current_art.rs
+++ server/artell-usecase/src/user/get_current_art.rs
@@ -1,11 +1,13 @@
 use artell_domain::{
     art::{Art, ArtRepository},
+    artist::{Artist, ArtistRepository},
     image::ImageRepository,
     scheduler::SchedulerRepository,
 };
 
 pub struct Res {
     pub art: Art,
+    pub artist: Artist,
     pub image_url: String,
 }
 
@@ -14,6 +16,7 @@ pub struct Res {
 pub async fn get_current_art(
     scheduler_repo: impl SchedulerRepository,
     art_repo: impl ArtRepository,
+    artist_repo: impl ArtistRepository,
     image_repo: impl ImageRepository,
 ) -> anyhow::Result<Option<Res>> {
     let scheduler = scheduler_repo
@@ -23,9 +26,17 @@ pub async fn get_current_art(
 
     if let Some(art_id) = scheduler.current_art_id().copied() {
         let art = art_repo.find_by_id(art_id.0).await?.expect("Infallible");
+        let artist = artist_repo
+            .find_by_id(art.artist_id.0)
+            .await?
+            .expect("Infallible");
         let image_url = image_repo.url_to(art.image_name.as_str());
 
-        Ok(Some(Res { art, image_url }))
+        Ok(Some(Res {
+            art,
+            artist,
+            image_url,
+        }))
     } else {
         Ok(None)
     }
