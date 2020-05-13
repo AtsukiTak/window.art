717e43557b83a4168ad5984f92682d454ae3625a
AtsukiTak <takatomgoo@gmail.com>
2020-05-13 14:52:13 +0900

    キャプションとリンクを適切に表示するように修正

--- chrome-extension/src/js/index.js
+++ chrome-extension/src/js/index.js
@@ -1,17 +1,27 @@
 window.onload = function() {
-
   fetch("https://artell.herokuapp.com/api/v1/user/get_current_art")
     .then(res => res.json())
     .then(json => {
-      var img = document.getElementById("works");
-      var caption = document.getElementById("caption");
+      var imgEle = document.getElementById("works");
+      var captionEle = document.getElementById("caption");
+      var titleEle = document.getElementById("title");
+      var artistEle = document.getElementById("artist");
 
-      img.setAttribute("src", json.imageUrl);
-      img.onload = function() {
-        img.classList.add("show");
+      // caption情報の設定
+      titleEle.textContent = json.artTitle;
+      artistEle.textContent = json.artistName;
+
+      // ポートフォリオへのリンクの設定
+      var encodedArtistName = encodeURI(json.artistName);
+      var portoflioUrl = `https://portfolio.artell.life/${encodedArtistName}/${json.portfolioId}/`;
+      document.getElementById("link").setAttribute("href", portoflioUrl);
+
+      // 画像の設定
+      imgEle.setAttribute("src", json.imageUrl);
+      imgEle.onload = function() {
+        imgEle.classList.add("show");
         // imgのwidthに合わせて、キャプションのwidthを決定する
-        caption.style.width = img.width + "px";
+        captionEle.style.width = imgEle.width + "px";
       };
-
     });
 };
